1b74f1d5ae047b6a623d1a55d4e1dddd6f9d07d0
Ansel-dal <82544423+Ansel-dal@users.noreply.github.com>
2022-09-14 16:04:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,3 +37,47 @@ sudo systemctl enable dcmdigitalsignage.service
 #inico servicio
 sudo systemctl start dcmdigitalsignage.service
 
+#creo el script para reiniciar al detectar ping
+echo -e "#!/bin/bash
+host=192.168.2.178
+ping_result="NOT_OK"
+until ping -q -c 4 $host >/dev/null
+do
+echo "No hay conexión"
+done
+sudo systemctl restart dcmlocker.service
+echo "reiniciado"
+
+
+" >> /home/pi/Desktop/script.sh
+
+sudo rm -r /etc/rc.local
+#creo el nuevo archivo rc.local
+echo -e "#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+# Print the IP address
+_IP=$(hostname -I) || true
+if [ "$_IP" ]; then
+  printf "My IP address is %s\n" "$_IP"
+fi
+
+bash /home/pi/Desktop/script.sh
+
+exit 0
+
+
+
+" >> /etc/rc.local
+
+
